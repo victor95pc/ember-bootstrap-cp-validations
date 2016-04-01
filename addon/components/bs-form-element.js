e32b98e87f31435ab0af7e8adfccf3d847384d80
Offir Golan <offirgolan@yahoo-inc.com>
2016-04-01 10:29:33 -0700

    fix spacing

--- addon/components/bs-form-element.js
+++ addon/components/bs-form-element.js
@@ -7,18 +7,18 @@ const {
 } = Ember;
 
 export default BsFormElement.extend({
-    _attrValidations: null,
-    notValidating: computed.not('isValidating').readOnly(),
-    notDisabled: computed.not('disabled').readOnly(),
+  _attrValidations: null,
+  notValidating: computed.not('isValidating').readOnly(),
+  notDisabled: computed.not('disabled').readOnly(),
 
-    // Overwrite
-    hasValidator: computed.notEmpty('_attrValidations').readOnly(),
-    hasErrors: computed.and('_attrValidations.isInvalid', 'notValidating').readOnly(),
-    isValidating: computed.readOnly('_attrValidations.isValidating'),
-    required: computed.and('_attrValidations.options.presence.presence', 'notDisabled'),
+  // Overwrite
+  hasValidator: computed.notEmpty('_attrValidations').readOnly(),
+  hasErrors: computed.and('_attrValidations.isInvalid', 'notValidating').readOnly(),
+  isValidating: computed.readOnly('_attrValidations.isValidating'),
+  required: computed.and('_attrValidations.options.presence.presence', 'notDisabled'),
 
-    setupValidations() {
-      defineProperty(this, '_attrValidations', computed.readOnly(`model.validations.attrs.${this.get('property')}`));
-      defineProperty(this, 'errors', computed.readOnly(`_attrValidations.messages`));
-    }
+  setupValidations() {
+    defineProperty(this, '_attrValidations', computed.readOnly(`model.validations.attrs.${this.get('property')}`));
+    defineProperty(this, 'errors', computed.readOnly(`_attrValidations.messages`));
+  }
 });
