104ad8c280e791f33c858f995b6e5e22edcc907a
Victor Palomo de Castro <victorpalomocastro@gmail.com>
2016-12-01 12:42:31 -0200

    unprivate attrValidations so you can set another validation outside the model

--- addon/components/bs-form-element.js
+++ addon/components/bs-form-element.js
@@ -3,23 +3,27 @@ import BsFormElement from 'ember-bootstrap/components/bs-form-element';
 
 const {
   computed,
-  defineProperty
+  defineProperty,
+  isBlank
 } = Ember;
 
 export default BsFormElement.extend({
-  _attrValidations: null,
+  attrValidations: null,
   notValidating: computed.not('isValidating').readOnly(),
   notDisabled: computed.not('disabled').readOnly(),
 
   // Overwrite
-  hasValidator: computed.notEmpty('_attrValidations').readOnly(),
-  hasErrors: computed.and('_attrValidations.isInvalid', 'notValidating').readOnly(),
-  isValidating: computed.readOnly('_attrValidations.isValidating'),
-  required: computed.and('_attrValidations.options.presence.presence', 'notDisabled'),
+  hasValidator: computed.notEmpty('attrValidations').readOnly(),
+  hasErrors: computed.and('attrValidations.isInvalid', 'notValidating').readOnly(),
+  isValidating: computed.readOnly('attrValidations.isValidating'),
+  required: computed.and('attrValidations.options.presence.presence', 'notDisabled'),
 
   setupValidations() {
-    defineProperty(this, '_attrValidations', computed.readOnly(`model.validations.attrs.${this.get('property')}`));
-    defineProperty(this, 'errors', computed.readOnly('_attrValidations.messages'));
-    defineProperty(this, 'warnings', computed.readOnly('_attrValidations.warningMessages'));
+    if (isBlank(this.get("attrValidations"))) {
+      defineProperty(this, 'attrValidations', computed.readOnly(`model.validations.attrs.${this.get('property')}`));
+    }
+    
+    defineProperty(this, 'errors', computed.readOnly('attrValidations.messages'));
+    defineProperty(this, 'warnings', computed.readOnly('attrValidations.warningMessages'));
   }
 });
